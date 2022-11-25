0a38d1e8b6433bf0956441c62819fbe57e75198e
Eduard Koshkelyan <eduardko2001@gmail.com>
2022-11-25 21:13:41 +0400

    fix(footer-author-link): eslint-disable

--- components/Reusables/Footer.tsx
+++ components/Reusables/Footer.tsx
@@ -3,8 +3,12 @@ import React from "react";
 const Footer = () => (
   <footer>
     <span>Developed by</span>
-    /* eslint-disable react/jsx-no-target-blank */
-    <a href="https://eduard-koshkelyan.onrender.com/" target="_blank" rel="noopener">
+    {/* eslint-disable react/jsx-no-target-blank */}
+    <a
+      href="https://eduard-koshkelyan.onrender.com/"
+      target="_blank"
+      rel="noopener"
+    >
       Eduard K.
     </a>
     😉
