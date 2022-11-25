5c1fc1077a9d77063012193a8184661ee39ea828
Eduard Koshkelyan <53624093+EdKo2001@users.noreply.github.com>
2022-11-25 21:00:17 +0400

    Update Footer.tsx

--- components/Reusables/Footer.tsx
+++ components/Reusables/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer>
     <span>Developed by</span>
-    <a href="https://eduard-koshkelyan.onrender.com/" target="_blank">
+    <a href="https://eduard-koshkelyan.onrender.com/" target="_blank" rel="noopener">
       Eduard K.
     </a>
     😉
